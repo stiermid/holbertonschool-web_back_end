620cf1f996fa3ced9bcfc97016923b9a89793a60
Agil Mammadov <mammadovagil@proton.me>
2026-06-14 14:03:31 +0400

    fix(pagination): pass def vals

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -36,11 +36,7 @@ class Server:
 
         return self.__indexed_dataset
 
-    def get_hyper_index(
-        self,
-        index: int = None,
-        page_size: int = 10
-    ) -> Dict:
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """Return a deletion-resilient hypermedia page"""
         dataset = self.indexed_dataset()
 
